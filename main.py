283a835d1ee8838a0cbd080c2b0e694a22faf0d2
fengwenni001 <fengwenni001@ke.com>
2023-08-23 19:31:08 +0800

    11

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ import os
 import shutil
 import zipfile
 import xml.etree.ElementTree as ET
-from xml.dom.minidom import parse   ## https://docs.python.org/zh-cn/3/library/xml.dom.minidom.html
+from xml.dom.minidom import parse   ##
+import xml.dom.minidom
+
+# https://docs.python.org/zh-cn/3/library/xml.dom.minidom.html
+
+
+# 节点库
+def dom_rep():
+    # body节点的子节点库
+    body_child = ['w:p','w:tbl','w:sdt','w:bookmarkStart','w:bookmarkEnd','w:sectPr']
+
 
 def docx_zip_unzip(root: str='', pre_files_dir:str='') -> None:
     # 遍历所有的子文档，进行压缩，暂存后解压
@@ -34,62 +44,61 @@ def docx_zip_unzip(root: str='', pre_files_dir:str='') -> None:
 
 # 遍历所有的xml文档，分析同级标签
 def parse_xml_first_tag(xml_dir:str=''):
+    # 定义方法内的全局变量
     tag_lst = []
     tag_lst_only = []
+    main_doms: [xml.dom.minidom.Element]  # body直接子节点
 
     # 验证docx的document.xml中的所有标签是否有不同库但同名的情况
     for root, dirs, files in os.walk(xml_dir):
+        dirs = sorted(dirs)
         for dir in dirs:
             document_file = xml_dir + dir + '\\word\document.xml'
             if os.path.exists(document_file):
-                # 解析XML文件
-                tree = ET.parse(document_file)
-                root = tree.getroot()
-
-                # 遍历XML文档
-                for elem in root.iter():
-                    if elem.tag not in tag_lst:
-                        tag_lst.append(elem.tag)
-                        tag_name = elem.tag.split('}')[1]
-                        # print(elem.tag)
-                        tag_lst_only.append(tag_name)
-    
-    if len(tag_lst) != len(tag_lst_only):
-        print('document.xml文件中存在标签同名不同库！')
-        print(len(tag_lst))
-        print(len(tag_lst_only))
-        return
-
-
-    # 筛选document.xml文件中<w:body>标签内所有同级标签
-    for root, dirs, files in os.walk(xml_dir):
-        for i, dir in enumerate(dirs):
-            document_file = xml_dir + dir + '\\word\document.xml'
-            if os.path.exists(document_file):
-                # 解析XML文件
-                tree = ET.parse(document_file)
-                # 判断所有的跟标签是否都为 document
-                root = tree.getroot()   # 根标签统一为document
-                if root.tag.split('}')[1] != 'document':
+                # 解析XML文件，获取文件根节点
+                dom_xml = parse(document_file)
+                root_dom = dom_xml.documentElement   # 获取文档唯一的根节点 <w:document>
+                root_dom_name = root_dom.nodeName
+                # 判断根节点是否唯一
+                if root_dom_name != 'w:document':
                     print(dir)
-                    print(root.tag)
-                    exit()
-                # 解析根标签的子标签
-                for child in root.iter('document'):
-                    print(child.tag)
+                    print("根节点不为w:document")
+                    return
 
 
-                # 遍历XML文档
-                # for elem in root.iter():
-                #     if elem.tag not in tag_lst:
-                #         tag_lst.append(elem.tag)
-                #         tag_name = elem.tag.split('}')[1]
-                #         # print(elem.tag)
-                #         tag_lst_only.append(tag_name)
+                # 获取根节点的唯一子节点：w:body
+                body_doms = root_dom.childNodes
+                if len(body_doms) > 1 or len(body_doms) == 0:
+                    print('根目录的直接子节点数量为' + str(len(body_doms)) + '个！')
+                    return
+                for x in body_doms:
+                    if x.nodeName != 'w:body':
+                        print(dir)
+                        print('根目录后的节点不是w:body')
+                        return
+                body_dom = body_doms[0]
 
-                # # 获取指定标签的元素值
-                # for elem in root.iter('tag_name'):
-                #     print(elem.text)
+                # 获取body节点的所有直接子节点, 直接子节点名称,存入节点库
+                main_doms = body_dom.childNodes
+                for i, main_dom in enumerate(main_doms):
+                    main_dom_name = main_dom.nodeName
+                    if main_dom_name not in tag_lst:
+                        tag_lst.append(main_dom_name)
+
+
+
+
+                for main_dom in main_doms:
+                    if
+
+
+
+        print('===================')
+        for x in tag_lst:
+            print(x)
+        print(len(tag_lst))
+
+
 
 
 
