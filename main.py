19550c09391d4f4f3bf087212717b556ca2280fd
fengwenni001 <fengwenni001@ke.com>
2023-08-25 11:13:45 +0800

    表格内容解析，并以md字符串返回

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ Time     : 2023/8/23
 Author   : Hillking
 File     : main.py
 Function : docx文档解析
+
+https://docs.python.org/zh-cn/3/library/xml.dom.minidom.html
 """
+
 import json
 import os
 import shutil
 import zipfile
 import xml.etree.ElementTree as ET
 from xml.dom.minidom import parse   ##
-import xml.dom.minidom
+import xml.dom.minidom as xdm
+import re
+from numering_parse import NumberParse
 
-# https://docs.python.org/zh-cn/3/library/xml.dom.minidom.html
+number_parse = NumberParse()
+number_parse.get_numbering('D:\Project\Dataset\排版-完整版财务知识\压缩包解压目录\\2 贝壳集团核算月结管理制度\word\\numbering.xml')
+# print(number_parse_exp.abstractnum_dict)
+# print(number_parse_exp.numid_absid_dict)
+# print(number_parse_exp.num_pointer)
+# exit()
+
+
 
 
 # 全局变量库
-dom_rely_dict : {str:list}= {}
+dom_rely_dict: {str:list} = {}
 
 
 # 节点库
@@ -50,7 +62,7 @@ def docx_zip_unzip(root: str='', pre_files_dir:str='') -> None:
                 zip_ref.extractall(unzipdir + file_name)
 
 
-def set_recurrent_dom(root_dom: xml.dom.minidom.Element) -> None:
+def set_recurrent_dom(root_dom: xdm.Element) -> None:
     """
     循环遍历节点，返回每种节点的所有子节点.
     其中：root_dom将作为父级节点来获取其直接子节点
@@ -77,7 +89,7 @@ def set_recurrent_dom(root_dom: xml.dom.minidom.Element) -> None:
             set_recurrent_dom(child_dom)
         return
 
-def get_recurrent_dom(root_dom: xml.dom.minidom.Element) -> None:
+def get_recurrent_dom(root_dom: xdm.Element) -> None:
     """
     循环遍历节点，返回每个节点的所有直接子节点列表.
     其中：root_dom将作为父级节点来获取其直接子节点
@@ -107,7 +119,7 @@ def parse_xml_first_tag(xml_dir:str='') -> None:
     # 定义方法内的全局变量
     tag_lst = []
     tag_lst_only = []
-    main_doms: [xml.dom.minidom.Element]  # body直接子节点
+    main_doms: [xdm.Element]  # body直接子节点
 
     # 验证docx的document.xml中的所有标签是否有不同库但同名的情况
     for root, dirs, files in os.walk(xml_dir):
@@ -155,55 +167,135 @@ def parse_xml_first_tag(xml_dir:str='') -> None:
         #     jsonw.write(dom_json)
 
 
-def get_and_analyse_dom(dom_name: str='') -> None:
-    child_dom_name = dom_rely_dict.get(dom_name)
-    print(child_dom_name)
 
 
+def get_content_from_tag_p(root_dom:xdm.Element, p_text:str='') -> str:
+    # TODO: 修改段落文本解析代码
+    '''
+    :param root_dom: 节点：不为空节点
+    :param p_text: 各阶段的文本
+    :return: 返回最终段落的文本
+    :func: 解析p标签内部子节点
+    '''
 
-father_dom = []
-grandfather_dom = []
-p_father_dom = []
-def get_content(root_dom:xml.dom.minidom.Element):
+    if root_dom is None:
+        return p_text
+
     root_dom_name  = root_dom.nodeName
     if root_dom_name == 'w:t':
         dom_text = root_dom.childNodes[0].data
-        print(dom_text)
+        p_text = p_text + dom_text
 
-        f_dom = root_dom.parentNode
-        g_dom = f_dom.parentNode
-        if f_dom.nodeName not in father_dom:
-            father_dom.append(f_dom.nodeName)
-        if g_dom.nodeName not in grandfather_dom:
-            grandfather_dom.append(g_dom.nodeName)
+    if root_dom_name == 'w:numPr':  # 编号解析
+        num_text = number_parse.get_parse_number_text(root_dom)
+        p_text = p_text + num_text + ' '
+        if p_text == '非中文类型文本暂未解析':
+            pass
 
-    if root_dom_name == 'w:p':
-        p_dom = root_dom.parentNode
-        if p_dom.nodeName not in p_father_dom:
-            p_father_dom.append(p_dom.nodeName)
 
     child_doms = root_dom.childNodes
     if len(child_doms) ==0:
-        return
+        return p_text
 
     for child_dom in child_doms:
-        get_content(child_dom)
+        p_text = get_content_from_tag_p(child_dom, p_text)
+    return p_text
 
 
+def get_content_from_tag_tb(root_dom:xdm.Element) -> [str, [str]]:
+    '''
+    :func: 解析表格中的文本并按照某种格式输出
+    :param root_dom: tbl表格节点，默认不为空节点
+    :return: 表格完整的文本串(同md)， 表格每一行的文本串列表(同md)
+    '''
+
+    if root_dom is None:
+        raise os.error(2, '未传入表格节点！===：None')
+    if root_dom.nodeName != 'w:tbl':
+        raise os.error(2, '表格节点类型错误！===：' + root_dom.nodeName)
+
+
+    tb_row_lst = []
+    tb_whole_text = ''
+    child_doms = root_dom.childNodes
+    for child_dom in child_doms:
+        child_dom_name = child_dom.nodeName
+        if child_dom_name == 'w:tr':   # 解析每一行的元素
+            tb_cel_doms = child_dom.childNodes
+            row_text = ''  # 每一行的文本初始化
+            for tb_cel_dom in tb_cel_doms:
+                tb_cel_dom_name = tb_cel_dom.nodeName
+                if tb_cel_dom_name == 'w:tc': # 解析每一行的每一列元素
+                    tb_p_doms = tb_cel_dom.getElementsByTagName('w:p')
+                    p_text = ''
+                    for tb_p_dom in tb_p_doms:
+                        p_text = get_content_from_tag_p(tb_p_dom, p_text)
+                    p_text = ' ' if p_text == '' else p_text
+                    row_text = row_text + ' | ' + p_text
+            row_text = row_text + ' | \n'
+            tb_whole_text = tb_whole_text + row_text
+            tb_row_lst.append(row_text)
+
+    return tb_whole_text, tb_row_lst
+
+
+
+def get_content_from_tag_sdt(root_dom:xdm.Element):
+    '''
+    :func: 从结构体中解析特定文本
+    :param root_dom:
+    :return:
+    '''
+
+    # TODO：补充代码
+    pass
 
 def get_text_from_dom(xml_path:str=''):
     dom_xml = parse(xml_path)
     root_dom = dom_xml.documentElement  # 获取文档唯一的根节点 <w:document>
     root_dom_name = root_dom.nodeName
 
-    get_content(root_dom)
+    get_content_from_tag_p(root_dom)
 
 
 
 
 
+def parse_different_dom(root_dom:xdm.Element):
+    '''
+    :param root_dom: 节点
+    :return:
+    :func: 不同类型的节点判断，并选择不同的节点处理方法
 
-def parse_docx_content(root_dom:xml.dom.minidom.Element):
+    节点分级：
+    body子节点：p\tbl\sdt
+    p内子节点：
+    '''
+
+    if root_dom is None:
+        return
+
+
+
+    if root_dom.nodeName == 'w:p':
+        p_text = get_content_from_tag_p(root_dom, '')  # 文本段落处理
+        print(p_text)
+    elif root_dom.nodeName == 'w:tbl':
+        get_content_from_tag_tb(root_dom)
+    elif root_dom.nodeName == 'w:sdt':
+        get_content_from_tag_sdt(root_dom)
+    # 待扩展的标签：图片、附件
+    # elif root_dom.nodeName == '':
+
+
+    # 子节点循环遍历
+    child_doms = root_dom.childNodes
+    if len(child_doms) == 0:
+        return
+    for child_dom in child_doms:
+        parse_different_dom(child_dom)
+
+
 
 
 
@@ -229,15 +321,10 @@ if __name__ == '__main__':
     # get_and_analyse_dom('w:p')
 
     # 4.获取标签中的全部文本
-    get_text_from_dom('D:\Project\Dataset\排版-完整版财务知识\压缩包解压目录\\2 贝壳集团核算月结管理制度\word\document.xml')
+    # get_text_from_dom('D:\Project\Dataset\排版-完整版财务知识\压缩包解压目录\\2 贝壳集团核算月结管理制度\word\document.xml')
 
-    print('========father_doms========')
-    print(father_dom)
-    print('========grandfather_doms========')
-    print(grandfather_dom)
-    print('========p_father_doms========')
-    print(p_father_dom)
-
-
-
+    # 6.解析所有的标签，获取文本内容
+    dom_xml = parse('D:\Project\Dataset\排版-完整版财务知识\压缩包解压目录\\2 贝壳集团核算月结管理制度\word\document.xml')
+    root_dom = dom_xml.documentElement  # 获取文档唯一的根节点 <w:document>
+    parse_different_dom(root_dom)
 
---
+++ numering_parse.py
@@ -0,0 +1,157 @@
+"""
+-*- coding: utf-8 -*-
+Time     : 2023/8/24
+Author   : Hillking
+File     : numering_parse.py
+Function : Parse numbering.xml
+"""
+import os.path
+from xml.dom.minidom import Element, parse
+import re
+
+class NumberParse:
+    numid_absid_dict: {str: str}  # numid 和 abstractNumId的映射关系
+    abstractnum_dict: {str: {str: {str: str}}}  # 每个abstractNumId对应的所有级别及级别的属性
+    num_pointer: {str: {str: str}}  # 每种abs中，某一种级别，对应的第几个编号
+
+    def __init__(self):
+        self.numid_absid_dict = {}
+        self.abstractnum_dict = {}
+        self.num_pointer = {}
+
+    def get_num_from_numbering_xml(self, root_dom: Element):
+        '''
+        :fuc: 解析numbering.xml文件，获取w:numbering节点的直接子节点的编号信息
+        :param root_dom:  必须不为空节点
+        '''
+
+        # 变量初始化
+        num_child_doms = root_dom.childNodes
+        for child_dom in num_child_doms:
+            child_dom_name = child_dom.nodeName
+            if child_dom_name == 'w:num':
+                numID = child_dom.getAttribute('w:numId')
+                abstractNumId = child_dom.childNodes[0].getAttribute('w:val')
+                self.numid_absid_dict[numID] = abstractNumId
+            elif child_dom_name == 'w:abstractNum':
+                abstractNumId = child_dom.getAttribute('w:abstractNumId')
+                level_dict: {str: {str: str}}  # 每个abstractNum的{级别: {属性}}
+                level_dict = {}
+
+                lvl_doms = child_dom.childNodes
+                for lvl_dom in lvl_doms:
+                    level_attr_dict: {str: str}
+                    level_attr_dict = {}
+                    lvl_dom_name = lvl_dom.nodeName
+                    if lvl_dom_name == 'w:lvl':
+                        lvl_dom_level = lvl_dom.getAttribute('w:ilvl')
+                        level_attr_dict['level'] = lvl_dom_level
+
+                        lvl_child_doms = lvl_dom.childNodes
+                        for lvl_child_dom in lvl_child_doms:
+                            lvl_child_dom_name = lvl_child_dom.nodeName
+                            if lvl_child_dom_name == 'w:start':
+                                start = lvl_child_dom.getAttribute('w:val')
+                                level_attr_dict['start'] = start
+                            elif lvl_child_dom_name == 'w:numFmt':
+                                numFmt = lvl_child_dom.getAttribute('w:val')
+                                level_attr_dict['numFmt'] = numFmt
+                            elif lvl_child_dom_name == 'w:lvlText':
+                                lvlText = lvl_child_dom.getAttribute('w:val')
+                                # pattern = "(%\d+)"
+                                # lvlText = re.sub(pattern, '*', lvlText)
+                                level_attr_dict['lvlText'] = lvlText
+
+                        level_dict[lvl_dom_level] = level_attr_dict
+
+                self.abstractnum_dict[abstractNumId] = level_dict
+
+    def get_numbering(self, numbering_xml_path: str = ''):
+        if not os.path.exists(numbering_xml_path) and not numbering_xml_path.endswith('.xml'):
+            raise os.error(2, "No such file or not a file which like numbering.xml: " + numbering_xml_path)
+
+        num_xml = parse(numbering_xml_path)
+        num_dom = num_xml.documentElement
+
+        try:
+            self.get_num_from_numbering_xml(num_dom)
+        except Exception as e:
+            raise os.error(2, "'numbering.xml' parsing error!: " + numbering_xml_path)
+
+    def get_parse_number_text(self, root_dom: Element) -> str:
+        '''
+        :param root_dom: 具有编号信息的节点。不能为空；必须为 w:numPr
+        :return: 返回编号节点对应的真实编号文本
+        '''
+        if root_dom is None:
+            raise os.error(2, '编号节点为空！===：None')
+
+        root_dom_name = root_dom.nodeName
+        lvlText_infact = ''
+        if root_dom_name == 'w:numPr':  # 编号解析
+            num_level = ''
+            num_id = ''
+
+            # 获取该节点中的编号级别和编号值
+            num_childs = root_dom.childNodes
+            for num_child in num_childs:
+                num_child_name = num_child.nodeName
+                if num_child_name == 'w:ilvl':
+                    num_level = num_child.getAttribute('w:val')
+                elif num_child_name == 'w:numId':
+                    num_id = num_child.getAttribute('w:val')
+
+
+            # 获取numid和absid的映射关系、编号级别对应的属性
+            absnum_id = self.numid_absid_dict.get(num_id)
+            abs_level_attr = self.abstractnum_dict.get(absnum_id).get(num_level)
+
+
+            start_attr = abs_level_attr.get('start')   # abs_level_attr 属性解析
+            numFmt_attr = abs_level_attr.get('numFmt')
+            lvlText_attr = abs_level_attr.get('lvlText')
+
+
+            start_num_infact = ''   # 获取实际编号值，并重置编号指针栏
+            if self.num_pointer.get(absnum_id):
+                if self.num_pointer.get(absnum_id).get(num_level):
+                    # 编号递增
+                    start_num_infact = self.num_pointer.get(absnum_id).get(num_level)
+                    next_num = int(start_num_infact) + 1
+                    self.num_pointer.get(absnum_id)[num_level] = str(next_num)  # 指针为下一个同级别的编号
+                else:
+                    # 编号重置
+                    start_num_infact = start_attr
+                    next_num = int(start_num_infact) + 1
+                    self.num_pointer.get(absnum_id)[num_level] = str(next_num)
+            else:
+                start_num_infact = start_attr
+                next_num = int(start_num_infact) + 1
+                level_value = {}
+                level_value[num_level] = str(next_num)
+                self.num_pointer[absnum_id] = level_value
+
+            # 解析真实的编号文本
+            if numFmt_attr == 'chineseCounting':
+                num_dict = {'0': '零', '1': '一', '2': '二', '3': '三', '4': '四', '5': '五', '6': '六',
+                            '7': '七', '8': '八', '9': '九'}
+                # unit_dict = {'0':'', '1':'十', '2':'百', '3':'千'}
+                # zh_num = ''
+                # if numFmt_attr == 'chineseCounting':
+                #     num_len = len(start_num_infact)
+                #     for num_i in range(0, num_len):
+                #         if num_i == num_len - 1 and start_num_infact[num_i: num_i + 1] == '0':
+                #             raise os.error(2, '编号错误！======：' + start_num_infact)
+                #         else:
+                #             zh_num = num_dict[start_num_infact[num_i: num_i + 1]] + unit_dict[str(num_len - num_i - 1)] + zh_num
+                #     # 参考：https://wenku.baidu.com/view/d1808dd566ce0508763231126edb6f1afe00710a.html?_wkts_=1692869928776&bdQuery=python%E5%B0%86%E6%95%B0%E5%AD%97%E8%BD%AC%E6%8D%A2%E6%88%90%E5%AF%B9%E5%BA%94%E7%9A%84%E4%B8%AD%E6%96%87%E5%AD%97%E7%AC%A6
+                pattern = "(%\d+)"
+                lvlText_infact = re.sub(pattern, num_dict.get(start_num_infact), lvlText_attr)
+            else:
+                # TODO:补充解析其他类型编号文本的代码
+                lvlText_infact = '非中文类型文本暂未解析'
+
+            return lvlText_infact
+        else:
+            raise os.error(2, '编号节点类型错误！===：' + root_dom_name)
+
